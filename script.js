6d02edde4d3b5a7de740a6ded0bfd3c507291f2f
pedro <pedrolucasgonza31@gmail.com>
2025-03-13 21:33:07 -0300

    teste js :)

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 //oi
 /*testando tipos de comentarios*/
 alert ("Bem vindo ao teste")
-confirm ("Você deseja c?")
-prompt ("Qual é o seu nome?")
+if(confirm("Voce utiliza java")){
+    alert("Macho")
+}
+else{
+    if(confirm("Voce utiliza python")){
+        alert("Queima de vez em quando")
+    }
+else{   
+    if(confirm("Voce utiliza Js?")){
+        alert("Macho moderno")
+    }
+else{
+    if(confirm("Voce utiliza go?")){
+        alert("UMMMMM amostradinho")
+    }
+else{
+    if(confirm("Voce utiliza react?")){
+        alert("Mora no vale")
+    }
+    
+}
+} 
+}
+}
