6ed3c21cf7eb33446ce88af5f418e683d882aba7
pedro <pedrolucasgonza31@gmail.com>
2025-03-13 21:40:52 -0300

    joguim

--- script.js
+++ script.js
@@ -5,11 +5,24 @@ prompt("Digite sua idade");
 alert("Seja bem vindo ao jogo de adivinhação");
 if(confirm("Deseja jogar?")){
     alert("Vamos começar");
-    else{
-        alert("Você é obrigado a jogar");
-    
+    var numero = Math.floor(Math.random() * 10);
+    var tentativas = 3;
+    var chute = parseInt(prompt("Digite um número de 0 a 10"));
+    while(tentativas > 0){
+        if(chute == numero){
+            alert("Parabéns, você acertou!");
+            break;
+        }else if(chute < numero){
+            alert("O número é maior");
+            tentativas--;
+        }else if(chute > numero){
+            alert("O número é menor");
+            tentativas--;
+        }
+        chute = parseInt(prompt("Digite um número de 0 a 10"));
+    }
+    if(tentativas == 0){
+        alert("Suas tentativas acabaram, o número era " + numero);
+    }
 }
-
-
-
 
