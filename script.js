724c9387d3d3bcc4f54be6588553f5ddee4e7d9d
pedro <pedrolucasgonza31@gmail.com>
2025-03-13 21:34:31 -0300

    up

--- script.js
+++ script.js
@@ -4,24 +4,4 @@ alert ("Bem vindo ao teste")
 if(confirm("Voce utiliza java")){
     alert("Macho")
 }
-else{
-    if(confirm("Voce utiliza python")){
-        alert("Queima de vez em quando")
-    }
-else{   
-    if(confirm("Voce utiliza Js?")){
-        alert("Macho moderno")
-    }
-else{
-    if(confirm("Voce utiliza go?")){
-        alert("UMMMMM amostradinho")
-    }
-else{
-    if(confirm("Voce utiliza react?")){
-        alert("Mora no vale")
-    }
-    
-}
-} 
-}
-}
+
