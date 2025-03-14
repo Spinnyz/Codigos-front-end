67535fb410b974608ddcf2edb12ba0112c25ebf6
pedro <pedrolucasgonza31@gmail.com>
2025-03-13 21:44:19 -0300

    up

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ if(confirm("Deseja jogar?")){
         chute = parseInt(prompt("Digite um número de 0 a 10"));
     }
     if(tentativas == 0){
-        alert("Fim   o número era " + numero);
+        alert("Fim  do o número era " + numero);
     }
 
 }
