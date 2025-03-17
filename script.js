15f82203fb69a678e2e4f00161140310b5071d4b
pedro <pedrolucasgonza31@gmail.com>
2025-03-16 21:00:11 -0300

    up

--- script.js
+++ script.js
@@ -2,8 +2,4 @@ x = prompt ("Digite um número");
 y = prompt ("Digite outro número");
 z = parseInt(x) + parseInt(y);
 alert("A soma dos números é: " + z);
-
-var x = 30;
-var y = 20;
-var z = x + y;
-console.log(z);
+alert("A subtração dos números é: " + (x - y));
