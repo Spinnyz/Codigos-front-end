d34b9d14f1d05c5b28e846adc47bdcd2e1f961c2
pedro <pedrolucasgonza31@gmail.com>
2025-03-16 21:00:32 -0300

    up

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ y = prompt ("Digite outro número");
 z = parseInt(x) + parseInt(y);
 alert("A soma dos números é: " + z);
 alert("A subtração dos números é: " + (x - y));
+alert("A multiplicação dos números é: " + (x * y));
