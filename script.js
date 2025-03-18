d7d9d0aca937740ef2fafbcf1f976d175997aadf
pedro <pedrolucasgonza31@gmail.com>
2025-03-18 19:25:47 -0300

    up

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let n1 = prompt ("To type the first number:"); //first number
 var n2 = prompt ("To type the second number:");// second number
-var sum = Number(n1) + Number(n2);
+var sum = n1.toString() + n2.toString
 alert ("The sum of the numbers is " +sum);
