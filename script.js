322d633234ddb19c928cdd64a87d0759d18e7244
pedro <pedrolucasgonza31@gmail.com>
2025-03-18 18:53:59 -0300

    up

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-alert ("Hello, welcome to my page!");
-var name = prompt ("What is your name?") //Quest the user name and save it in a variable
-alert ("Hello", +name + " Nice to meet you!"); //Show a welcomer mensager 
+let 
