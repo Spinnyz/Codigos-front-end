6c3355cb8e0b6815c1d57d88546ff83b9e8f3534
pedro <pedrolucasgonza31@gmail.com>
2025-03-18 20:31:42 -0300

    up

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var name = prompt("Whats your name?");
-document.write(`Your name ${name} has ${name.length}`);
-document.write (`Your name in uppercase is ${name.UpperCase()}`);
-document.write(`Your name in lowercase is ${name.LowerCase()}`);
+document.write(`Your name ${name} has ${name.length} letters <br>`);
+document.write (`Your name in uppercase is ${name.toUpperCase()} <br>`);
+document.write(`Your name in lowercase is ${name.toLowerCase()}<br>`);
