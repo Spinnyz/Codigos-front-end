12cbfa607130ae498c9e440bc348ce1ad00ce757
pedro <pedrolucasgonza31@gmail.com>
2025-03-18 19:18:45 -0300

    up

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-let n1 = prompt ("To type the first number");
-var n2 = prompt ("To type the second number");
-alert ("The sum of the numbers is ")
+let n1 = prompt ("To type the first number:"); //first number
+var n2 = prompt ("To type the second number:");// second number
+var sum = parseFloat (n1) + parseFloat(n2);
+alert ("The sum of the numbers is " +sum);
