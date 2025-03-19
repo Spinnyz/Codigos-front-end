2825e1c876c58c42a25d745937ced65f9e9e7f10
pedro <pedrolucasgonza31@gmail.com>
2025-03-18 21:03:36 -0300

    u´p

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ alert ("Welcome to a your new job");
 var name = prompt ("Whats is your name?");
 var age = prompt ("How old are you?");
 var money = prompt ("How much  salary do you want do receive?");
-var x= Number(age) - Number (2025)
+var x =   2025 - Number(age);
 
 document.write (`For confirm, your name is ${name} your name have ${name.length} <br> in  upperscale is ${name.toUpperCase()} <br> in lowercase is ${name.toLowerCase()} <br> your age is ${age} then your born in ${x} <br> and how much salary to want is ${money.toLocaleString ("pt-BR", {style:"currency", currency: "BRL"})}`)
