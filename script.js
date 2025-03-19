14ba5ffb296c0fdd0a098aebaa4de3e305648c2c
pedro <pedrolucasgonza31@gmail.com>
2025-03-18 21:00:54 -0300

    up

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var name = prompt("Whats your name?");
-document.write(`Your name ${name} has ${name.length} letters <br>`);
-document.write (`Your name in uppercase is ${name.toUpperCase()} <br>`);
-document.write(`Your name in lowercase is ${name.toLowerCase()}<br>`);
+alert ("Welcome to a your new job");
+var name = prompt ("Whats is your name?");
+var age = prompt ("How old are you?");
+var money = prompt ("How much  salary do you want do receive?");
+var x= Number(age) - numeber (2025)
+
+document.write (`For confirm, your name is ${name} your name have ${name.length} <br> in  upperscale is ${name.toUpperCase()} <br> in lowercase is ${name.toLowerCase()} <br> your age is ${age} then your born in ${x} <br and how much salary to want is ${money.toLocaleString ("pt-BR", {style:"currency", currency: "BRL"})}`)
