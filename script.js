a4d36ed773c8ff42951ba38d2a335be460594230
pedro <pedrolucasgonza31@gmail.com>
2025-03-18 21:02:21 -0300

    up

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ var age = prompt ("How old are you?");
 var money = prompt ("How much  salary do you want do receive?");
 var x= Number(age) - Number (2025)
 
-document.write (`For confirm, your name is ${name} your name have ${name.length} <br> in  upperscale is ${name.toUpperCase()} <br> in lowercase is ${name.toLowerCase()} <br> your age is ${age} then your born in ${x} <br and how much salary to want is ${money.toLocaleString ("pt-BR", {style:"currency", currency: "BRL"})}`)
+document.write (`For confirm, your name is ${name} your name have ${name.length} <br> in  upperscale is ${name.toUpperCase()} <br> in lowercase is ${name.toLowerCase()} <br> your age is ${age} then your born in ${x} <br> and how much salary to want is ${money.toLocaleString ("pt-BR", {style:"currency", currency: "BRL"})}`)
