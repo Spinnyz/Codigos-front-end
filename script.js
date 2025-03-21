ddfb76f0b2ddec7db6393acb2c665f25150b3fc9
pedro <pedrolucasgonza31@gmail.com>
2025-03-20 21:45:31 -0300

    up

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ var password = prompt("What's your password?");
 var welcome = document.querySelector("p.Bem-vindo");
 welcome.innerText = `Bem vindo ${name}`;
 
-var le = document.querySelector
+var le = document.querySelector ("p.name")
+le.innertext = `Seu nome tem ${name.length} caracteres`;
 
 var age = document.querySelector("p.age")
 age.innerText = `Você tem ${idade} anos`;
