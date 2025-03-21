14d7591ee727cb7226c193ed6fb2d4bb3418b821
pedro <pedrolucasgonza31@gmail.com>
2025-03-21 20:07:55 -0300

    up

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ var mail = prompt("What's your email?");
 var assword = prompt("What's your password?");
 
 var welcome = document.querySelector("p.Bem-vindo");
-welcome.innerText = `Bem vindo ${namo}`;
+welcome.innerText = `Bem vindo: ${namo}`;
 
 var le = document.querySelector ("p.name")
-le.innerText = `Seu nome tem ${namo.length} caracteres`;
+le.innerText = `Seu nome tem: ${namo.length} caracteres`;
 
 var age = document.querySelector("p.age")
-age.innerText = `Você tem ${idade} anos`;
+age.innerText = `Você tem: ${idade} anos`;
 
 var emai = document.querySelector("p.email")
-emai.innerText = `Seu email é ${mail}`;
+emai.innerText = `Seu email é: ${mail}`;
 
 var pass= document.querySelector("p.password")
-pass.innerText = `Sua senha é ${assword}`;
+pass.innerText = `Sua senha é: ${assword}`;
 
