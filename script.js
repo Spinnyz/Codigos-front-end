579aceb99e1decf1410b67e9add5db166af788f2
pedro <pedrolucasgonza31@gmail.com>
2025-03-20 21:47:01 -0300

    up

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var name = prompt("Hello, What's your name?");
+var namo = prompt("Hello, What's your name?");
 var idade = prompt("How old are you?");
 var email = prompt("What's your email?");
 var password = prompt("What's your password?");
 
 var welcome = document.querySelector("p.Bem-vindo");
-welcome.innerText = `Bem vindo ${name}`;
+welcome.innerText = `Bem vindo ${namo}`;
 
 var le = document.querySelector ("p.name")
-le.innertext = `Seu nome tem ${name.length} caracteres`;
+le.innertext = `Seu nome tem ${namo.length} caracteres`;
 
 var age = document.querySelector("p.age")
 age.innerText = `Você tem ${idade} anos`;
