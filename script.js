2dac09d961928f666281faafcc5797345fbcf43a
pedro <pedrolucasgonza31@gmail.com>
2025-03-20 21:37:14 -0300

    up

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var name = prompt("Hello, What's your name?");
+var idade = prompt("How old are you?");
+var email = prompt("What's your email?");
+var password = prompt("What's your password?");
 
-var welcome = document.querySelector("p.resultado");
+var welcome = document.querySelector("p.Bem-vindo");
 welcome.innerText = `Bem vindo ${name}`;
