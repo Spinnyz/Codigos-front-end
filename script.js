c250ad9db92fd45fe6cb760ce507ff13213e696d
pedro <pedrolucasgonza31@gmail.com>
2025-03-21 20:45:58 -0300

    up

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function clica (){
+function clicar (){
     var int = document.querySelector("div#area");
     int.innerHTML = "Clicou!";
     int.style.background = "red";
     
 }
 
-function entra (){
+function entrar (){
     var int = document.querySelector("div#area");
     int.innerHTML = "Entrou!";
     int.style.background = "green";
+}
+
+function sair (){
+    var int = document.querySelector("div#area");
+    int.innerHTML = "Saiu!";
+    int.style.background = "blue";
 }
