12827b6adb1d30a28bab56418d385e9ecea41dc4
pedro <pedrolucasgonza31@gmail.com>
2025-03-21 21:04:39 -0300

    up

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var a = document.querySelector("div#area");
 a.addEventListener("click", clicar);
 a.addEventListener("mouseenter", entrar);
 a.addEventListener("mouseout", sair);
+a.addEventListener("mousemove", mover);
+a.addEventListener("mousedown", apertar);
+a.addEventListener("mouseup", soltar);
 
 
 function clicar (){
@@ -21,4 +24,22 @@ function sair (){
     var int = document.querySelector("div#area");
     int.innerHTML = "Saiu!";
     int.style.background = "blue";
+}
+
+function mover (){
+    var int = document.querySelector("div#area");
+    int.innerHTML = "Moveu!";
+    int.style.background = "yellow";
+}
+
+function apertar (){
+    var int = document.querySelector("div#area");
+    int.innerHTML = "Apertou!";
+    int.style.background = "black";
+}
+
+function soltar (){
+    var int = document.querySelector("div#area");
+    int.innerHTML = "Soltou!";
+    int.style.background = "white";
 }
