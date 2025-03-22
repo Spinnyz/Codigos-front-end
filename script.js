a5d7805d42bf58f48321f2969c20132e2d2f1323
pedro <pedrolucasgonza31@gmail.com>
2025-03-21 21:02:13 -0300

    up

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var a = document.querySelector("div#area");
+a.addEventListener("click", clicar);
+a.addEventListener("mouseenter", entrar);
+a.addEventListener("mouseout", sair);
+
+
 function clicar (){
     var int = document.querySelector("div#area");
     int.innerHTML = "Clicou!";
