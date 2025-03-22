09545cb20f18db635770f72f58af03fe61871c13
pedro <pedrolucasgonza31@gmail.com>
2025-03-21 23:42:07 -0300

    up

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 var nm1 = document.querySelector("input#inumber");
 var nm2 = document.querySelector("input#inumber2");
+var resultado = document.querySelector("input.resultado");
 
-document.write(nm1.innerHTML)
+function somar() {
+    var n1 = Number(nm1.value);
+    var n2 = Number(nm2.value);
+    var r = n1 + n2;
+}
+
+document.querySelector("button").addEventListener("click", somar);
