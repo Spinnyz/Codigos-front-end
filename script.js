497ffa8fa5d4606feb60d0e18b2630f60477c98a
pedro <pedrolucasgonza31@gmail.com>
2025-03-21 23:48:30 -0300

    up

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 var nm1 = document.querySelector("input#inumber");
 var nm2 = document.querySelector("input#inumber2");
-var resultado = document.querySelector("input.resultado");
+var resultado = document.querySelector("p.resultado"); // Changed to p.resultado
+
+var botao = document.querySelector("input.butão"); // Get the button
+
+botao.addEventListener("click", somar); // Add event listener to the button
 
 function somar() {
-    var n1 = Number(nm1.value);
-    var n2 = Number(nm2.value);
-    var r = n1 + n2;
-}
+  var n1 = Number(nm1.value);
+  var n2 = Number(nm2.value);
+  var soma = n1 + n2;
 
-document.querySelector("button").addEventListener("click", somar);
+  if (isNaN(n1) || isNaN(n2)) {
+    resultado.innerHTML = "O resultado é: Por favor, insira números válidos.";
+  } else {
+    resultado.innerHTML = `O resultado é: ${soma}`;
+  }
+}
