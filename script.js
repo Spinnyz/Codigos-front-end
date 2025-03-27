8213d3a7490467d17ef32a9009467c03176af2fa
pedro <pedrolucasgonza31@gmail.com>
2025-03-27 19:21:52 -0300

    up

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 var passa = document.querySelector("div.teste")
 
-function teste() {
+function entra() {
     passa.style.backgroundColor = "red"
+    passa.innerHTML = "Entrou"
 }
 
-passa.addEventListener("mouseover", teste)
+passa.addEventListener("mouseover", entra)
 
+
+function sai (){
+    passa.style.backgroundColor = "blue"
+    passa.innerHTML= "Saiu"
+}
+
+passa.addEventListener("mouseout", sai)
+
+function clica (){
+    passa.style.backgroundColor = "green"
+    passa.innerHTML= "soltou"
+}
+
+passa.addEventListener("click", clica)
+
+function solta (){
+    passa.style.backgroundColor = "yellow"
+    passa.innerHTML= "Clicou"
+}
+
+passa.addEventListener("mouseup", solta)
+
+function pressiona (){
+    passa.style.backgroundColor = "purple"
+    passa.innerHTML= "Pressionou"
+}
+
+passa.addEventListener("mousedown", pressiona)
