95f3705cf668d3c4b0f701d5d486176002e1224a
pedro <pedrolucasgonza31@gmail.com>
2025-03-28 18:56:39 -0300

    up

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var butão = document.querySelector("input#ibutton");
 var resultado = document.querySelector("p.resultado");
 
 function result (){
-    resultado.innerHTML = ("Usuario já existe");
+    resultado.innerHTML = ("O resultado é: Usuario já existe");
 }
 
+butão.addEventListener("click", result);
