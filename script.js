aa7eab64152eb71022a1f30d5bfed7935f1708e5
pedro <pedrolucasgonza31@gmail.com>
2025-03-30 19:32:48 -0300

    up

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-var butão = document.querySelector("input#isoma");
 
+//Calculadora simple
+
+var bsoma = document.querySelector("input#isoma");
+var bsub = document.querySelector("input#isub");
+var bmult = document.querySelector("input#imult");
+var bdiv = document.querySelector("input#idiv");
 var resultado = document.querySelector("p.resultado");
-
 var valor1 = document.querySelector("input#inumber1");
-
 var valor2 = document.querySelector("input#inumber2");
 
-function soma (){
+function soma() {
     var v1 = Number(valor1.value);
-
     var v2 = Number(valor2.value);
-
     var s = v1 + v2;
-
     resultado.innerHTML = `O resultado da soma entre ${v1} e ${v2} é igual a: ${s}`;
-} 
+}
 
-butão.addEventListener("click", soma);
+bsoma.addEventListener("click", soma);
 
+function sub(){
+    var v1 = Number(valor1.value);
+    var v2 = Number(valor2.value);
+    var s = v1 - v2;
+    resultado.innerHTML = `O resultado da subtração entre ${v1} e ${v2} é igual a: ${s}`;
+}
 
