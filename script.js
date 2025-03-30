f970d114a1c40b6223e5d434aafe76acd3de3296
pedro <pedrolucasgonza31@gmail.com>
2025-03-30 18:42:07 -0300

    up

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ var butão = document.querySelector("input#ibutton");
 
 var resultado = document.querySelector("p.resultado");
 
+var valor1 = document.querySelector("input#number1");
+
+var valor2 = document.querySelector("input#number2");
+
+var soma = Number(valor1.value) + Number(valor2.value);
+
 function result (){
-    resultado.innerHTML = ("O resultado é: Usuario já existe");
+    resultado.innerHTML = `O resultado é ${soma}`;
 }
 
 butão.addEventListener("click", result);
