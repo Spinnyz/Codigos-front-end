69d57a42092cca85ef2a07494553c29682f0936b
pedro <pedrolucasgonza31@gmail.com>
2025-03-30 21:12:56 -0300

    up

--- script.js
+++ script.js
@@ -51,3 +51,4 @@ bdiv.addEventListener("click", div)
 //Calculadora de IMC
 var peso = document.querySelector("input#peso");
 var altura = document.querySelector("input#altura");
+var bcalcular = document.querySelector("input#calcular");
