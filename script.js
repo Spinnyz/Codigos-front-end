4ddcc0d4eeea767db7b55c7313ba0ed731ebddd8
pedro <pedrolucasgonza31@gmail.com>
2025-04-10 21:54:59 -0300

    up

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-var fundo = document.querySelector("body")
-var cback = document.querySelector ("h1.cback")
-var button = document.querySelector ("button.botao")
+var fundo = document.querySelector("body");
+var cback = document.querySelector("h1.cback");
+var button = document.querySelector("button.botao");
 
-function mudarcor (){
-    var valor = Math.floor(Math.random()*9)+1
-    if(valor == 1){
-        fundo.style.backgroundColor = "red"
-    }
-    }else if (valor == 2){
-        fundo.style.backgroundColor = "blue"
-    }
-    else if (valor == 3){
-        fundo.style.backgroundColor = "yellow"
-    }
-    else if (valor == 4){
-        fundo.style.backgroundColor = "green"
+function mudarcor() {
+    var valor = Math.floor(Math.random() * 9) + 1;
+
+    if (valor == 1) {
+        fundo.style.backgroundColor = "red";
+    } else if (valor == 2) {
+        fundo.style.backgroundColor = "blue";
+    } else if (valor == 3) {
+        fundo.style.backgroundColor = "yellow";
+    } else if (valor == 4) {
+        fundo.style.backgroundColor = "green";
+    } else {
+        fundo.style.backgroundColor = "black";
+        button.style.color = "white"
+        
     }
 
-    
+ 
 }
 
-
-button.addEventListener("click", mudarcor)
-
-
+button.addEventListener("click", mudarcor);
 
 
