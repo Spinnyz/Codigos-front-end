5af4741a7d8e1f9608061cce4e78dd9d0e3ee2bf
pedro <pedrolucasgonza31@gmail.com>
2025-04-10 21:52:31 -0300

    up

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 var fundo = document.querySelector("body")
 var cback = document.querySelector ("h1.cback")
-var button = document.querySelector ("button.button")
+var button = document.querySelector ("button.botao")
 
 function mudarcor (){
     var valor = Math.floor(Math.random()*9)+1
-    alert ("oi")
+    if(math == 1){
+        fundo.style.backgroundColor = "red"
+    }
+    }else if (valor == 2){
+        fundo.style.backgroundColor = "blue"
+    }
+    else if (valor == 3){
+        fundo.style.backgroundColor = "yellow"
+    }
+    else if (valor == 4){
+        fundo.style.backgroundColor = "green"
+    }
+    
     
 }
 
