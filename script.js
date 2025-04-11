f0792a32de3b8f62177f46df4d0b011043ba8c65
pedro <pedrolucasgonza31@gmail.com>
2025-04-10 21:19:18 -0300

    up

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-var velo = document.querySelector("input#ikm")
-console.log(velo)
+var ikm = document.querySelector("input#ikm");
+var bkm = document.querySelector("input#ienviar");
+var vatual = document.querySelector("#velocidade");
+
+function resultado() {
+    var velocidade = Number(ikm.value); 
+    vatual.innerHTML = `Sua velocidade é de ${velocidade} Km/h`; 
+}
+
+bkm.addEventListener("click", resultado);
